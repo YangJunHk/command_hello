b854ed48771e51dec90db9b8ce5c8adf2efd23b9
JHYANG <162940406+YangJunHk@users.noreply.github.com>
2024-04-01 11:45:26 +0900

    Github for git pull

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@
 // First: Github modification
 // Second: Local repository modification
 
+>>>>>>> origin/master
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
